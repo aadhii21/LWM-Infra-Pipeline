653374e95188b1d2b8f39fec93e52044e9448ea4
aadhii21 <lalithadhi18@gmail.com>
2026-06-25 09:20:31 +0530

    Added terraform files and Jenkins file

---
+++ environments/dev/backend.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+        source ="hashicorp/aws"
+        version "~>6.50"
+    }
+  }
+  backend "s3" {
+    bucket= "lalithterraform21"
+    key ="environments/dev/terraform.tfstate "
+    region = "us-southeast-1"
+
+  }
+  
+}
+provider "aws"{
+    region =var.aws_region
+}
---
+++ environments/dev/main.tf
@@ -0,0 +1,14 @@
+module vpc {
+    source = "../../modules/vpc"
+    vpc_cidr = "10.0.0.0/16"
+    subnet_cidr = "10.0.1.0/24"
+    availability_zone = "${var.region}a"
+    env ="dev"
+}
+module ec2 {
+    source = "../../modules/ec2"
+    ami_id ="ami-095bd4a11ce8746c0"
+    instance_type = "t2.micro"
+    subnet_id = module.VPC.subnet_id
+}
+ 
