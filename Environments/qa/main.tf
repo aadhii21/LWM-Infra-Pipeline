fa4a83514eddd9c1eaf16144f93882ba18a18e93
aadhii21 <lalithadhi18@gmail.com>
2026-06-26 09:55:09 +0530

    ah ahhh

--- Environments/qa/main.tf
+++ Environments/qa/main.tf
@@ -1,12 +1,12 @@
 module vpc {
-    source = "../../modules/vpc"
+    source = "../../modules/VPC"
     vpc_cidr = "10.0.0.0/16"
     subnet_cidr = "10.0.1.0/24"
     availability_zone = "${var.region}a"
     env ="qa"
 }
 module ec2 {
-    source = "../../modules/ec2"
+    source = "../../modules/EC2"
     ami_id ="ami-095bd4a11ce8746c0"
     instance_type = "t2.micro"
     subnet_id = module.VPC.subnet_id
