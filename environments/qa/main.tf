107fdcee0a29161cfa670509ee21de03b2fdc24e
aadhii21 <lalithadhi18@gmail.com>
2026-06-27 15:22:47 +0530

    QA

--- environments/qa/main.tf
+++ environments/qa/main.tf
@@ -3,7 +3,7 @@ module vpc {
     vpc_cidr = "10.0.0.0/16"
     subnet_cidr = "10.0.1.0/24"
     availability_zone = "${var.region}a"
-    env ="qa"
+    Env ="qa"
 }
 module ec2 {
     source = "../../modules/EC2"
